7e5a7bd11df7a875269cdcb9a3f3c364a3c022cc
Samantha Emily-Rachel Belnavis <me@samantharachelb.me>
2018-10-08 19:50:11 -0400

    Added line to suppress warnings [ci skip]

--- Pods/Target Support Files/Pods-Fordly/Pods-Fordly-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Fordly : NSObject
-@end
-@implementation PodsDummy_Pods_Fordly
-@end
--- Pods/Target Support Files/Pods-FordlyTests/Pods-FordlyTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FordlyTests : NSObject
-@end
-@implementation PodsDummy_Pods_FordlyTests
-@end
